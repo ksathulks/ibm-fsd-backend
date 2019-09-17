4127941c844d5dd914dfad7b5bf1203b1748930e
ksathulks <53561870+ksathulks@users.noreply.github.com>
2019-09-18 00:49:36 +0530

    Employee Management App initial

--- assignments/corejava/employee-mgmt-app01/src/EmpService.java
+++ assignments/corejava/employee-mgmt-app01/src/EmpService.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -9,7 +15,7 @@ public class EmpService {
 		Scanner sc=new Scanner(System.in);
 		int choice;
 		do {
-			System.out.println("Enter 1 to add, 2 to view, 3 to update, 4 to delete, 5 to view all, 10 to exit");
+			System.out.println("Enter 1 to add, 2 to view, 3 to update, 4 to delete, 5 to view all, 6 to import, 7 to export, 10 to exit");
 			choice = sc.nextInt();
 			switch(choice) {
 				case 1:
@@ -32,6 +38,12 @@ public class EmpService {
 					break;
 				case 5:
 					viewAll();
+					break;
+				case 6:
+					importEmp();
+					break;
+				case 7:
+					exportEmp();
 					break;
 				case 10:
 					System.out.println("Exiting...");
@@ -88,6 +100,50 @@ public class EmpService {
 		empList.forEach(e->{
 			System.out.println(e);
 		});
+	}
+	public void exportEmp() {
+		try {
+			FileOutputStream fileOut = new FileOutputStream("C:\\Training\\ibm-fsd-backend\\assignments\\corejava\\employee-mgmt-app01\\newFile.txt");
+			empList.forEach(e->{
+				try {
+					String objlist = String.format("%d,%s,%d,%s,%s,%s", e.getId(),e.getName(),e.getAge(),e.getDesign(),e.getDept(),e.getCountry());
+					System.out.println(objlist);
+					fileOut.write(objlist.getBytes());
+					fileOut.write("\n".getBytes());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			});
+			fileOut.close();
+		}
+		catch (IOException i){
+			i.printStackTrace();
+		}
+		
+	}
+	
+	public void importEmp() {
+		Scanner s= new Scanner(System.in);
+        try {
+        	s = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Training\\ibm-fsd-backend\\assignments\\corejava\\employee-mgmt-app01\\newFile.txt"))));
+        	
+            while (s.hasNextLine()) {
+		        String[] empArr = s.nextLine().split(",");    	
+//                System.out.println(s.nextLine());
+                
+                Employee eNew= new Employee(Integer.parseInt(empArr[0]), empArr[1], Integer.parseInt(empArr[2]), empArr[3], empArr[4], empArr[5] );
+                empList.add(eNew);
+            }
+            viewAll();
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+		
+        		
 	}
 	
 	
--- assignments/corejava/employee-mgmt-app01/src/Employee.java
+++ assignments/corejava/employee-mgmt-app01/src/Employee.java
@@ -1,5 +1,6 @@
+import java.io.Serializable;
 
-public class Employee {
+public class Employee implements Serializable {
 	String name;
 	int age;
 	int id;
@@ -7,11 +8,42 @@ public class Employee {
 	String dept;
 	String country;
 	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getDesign() {
+		return design;
+	}
+	public void setDesign(String design) {
+		this.design = design;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
 	}
 	public Employee(int id, String name, int age, String design, String dept, String country) {
 		super();
